44f092e4f13e037689d5e0f6651377cee86ee3df
Hannes Van De Vreken <vandevreken.hannes@gmail.com>
2014-10-10 16:17:59 +0200

    Split of a 404 template for when a project name is not used in configuration

---
+++ resources/views/404.blade.php
@@ -0,0 +1,15 @@
+<!doctype html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <title>Days since...</title>
+  <link rel="stylesheet" href="css/style.css" type="text/css">
+</head>
+<body>
+  <div class="welcome">
+    <h2>
+      There is no such project: {{ $project_name }}
+    </h2>
+  </div>
+</body>
+</html>
